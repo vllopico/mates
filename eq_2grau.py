5939bd507889c61db6fcbfecab54513bfa00a759
vllopico <70440969+vllopico@users.noreply.github.com>
2020-09-01 22:14:32 +0200

    Add files via upload

---
+++ eq_2grau.py
@@ -0,0 +1,32 @@
+import math
+
+print("ax^2 + bx + c = 0")
+a = float(input("Coeficient a: "))
+b = float(input("Coeficient b: "))
+c = float(input("Coeficient c: "))
+
+print("Equació segon grau tipus -> "+str(a)+"x²+"+str(b)+"x+"+str(c)+"=0")
+
+
+if (a == 0):
+	if (b != 0):
+		x_1 = -c/b
+		print("Una solució")
+		print("x_1 --> ", x_1)
+	else:
+		if (c != 0):
+			print("Sense solució c = 0")
+		else:
+			print("0 = 0")
+else:
+	dis = b*b - 4*a*c
+	if (dis >= 0):
+		x_1 = (-b + math.sqrt(dis)) / 2*a
+		x_2 = (-b - math.sqrt(dis)) / 2*a
+		print("Dues solucions")
+		print("x_1 --> ", x_1)
+		print("x_2 --> ", x_2)
+		if (b == 0):
+			print("Infinites solucions, es mostren dos")
+	else:
+		print("No hi ha solució en R, buscar en C")
