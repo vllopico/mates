251a958a0155c1b6f6edc68b5a2fc2cc9c95d046
vllopico <70440969+vllopico@users.noreply.github.com>
2020-09-02 22:44:02 +0200

    Add files via upload
    Programa que calcula el factorial de n (un sencer major que 0) mitjançant la funció recursiva.

---
+++ factorial_recursiu.py
@@ -0,0 +1,19 @@
+import sys
+
+#Funció recursiva per a calcular el factorial de n
+def factorial(n):
+	if (n == 0):
+		return 1
+	else:
+		return n * factorial(n-1)
+
+try:
+	num = int(input("Indica el nombre sencer per a calcular el seu factorial: "))
+	if (num < 0):
+		negatiu = ValueError()
+		raise negatiu
+except ValueError:
+	print("L'entrada ha de ser un nombre sencer > 0")
+	sys.exit(1)
+
+print(f"{num}! = {factorial(num)}")
