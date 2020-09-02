2c3c83b97abf9c95a03be860ba6019aaa1021060
vllopico <70440969+vllopico@users.noreply.github.com>
2020-09-02 22:22:12 +0200

    Update nombre_perfecte.py

--- nombre_perfecte.py
+++ nombre_perfecte.py
@@ -1,6 +1,13 @@
 #Conèixer si un nombre sencer es perfecte 
 #Un nombre sencer és perfecte si la suma dels seus divisors es el mateix nombre
-num = int(input("Indica el nombre: "))
+
+import sys
+
+try:
+	num = int(input("Indica el nombre: "))
+except:
+	print("Error -> l'entrada ha de ser un nombre sencer")
+	sys.exit(1)
 
 div = 1
 suma = 0
@@ -14,4 +21,3 @@ if (num == suma):
 	print(f'{num} és un nombre perfecte')
 else:
 	print(f'{num} no és un nombre perfecte')
-	
