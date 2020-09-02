eefac323ccea2f37e440cacd32bb5a31d903bdea
vllopico <70440969+vllopico@users.noreply.github.com>
2020-09-02 22:46:44 +0200

    Update factorial_recursiu.py

--- factorial_recursiu.py
+++ factorial_recursiu.py
@@ -13,7 +13,7 @@ try:
 		negatiu = ValueError()
 		raise negatiu
 except ValueError:
-	print("L'entrada ha de ser un nombre sencer > 0")
+	print("L'entrada ha de ser un nombre natural ( >= 0)")
 	sys.exit(1)
 
 print(f"{num}! = {factorial(num)}")
